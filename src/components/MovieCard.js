9ac38ea8e6c213874c776c854ffc06787de8e627
Nurhanfront <front@studionomad.kz>
2021-07-13 15:39:39 +0600

    test task movie search

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -6,7 +6,6 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles({
   root: {
@@ -21,26 +20,26 @@ export default function MovieCard(props) {
   const classes = useStyles();
 
   return (
-      <Router>
-        <Card className={classes.root}>
-          <CardActionArea>
-            <CardMedia
-              className={classes.media}
-              image={props.movies.Poster}
-              title="Contemplative Reptile"
-            />
-            <Link to="/Nurhan">Home</Link>
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                {props.movies.Title}
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      </Router>
+    <route path="/card">
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={props.movies.Poster}
+            title="Contemplative Reptile"
+          />
+          <Link to="/Nurhan">Home</Link>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {props.movies.Title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </route>
   );
 }
